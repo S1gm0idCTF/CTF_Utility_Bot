d69cf05fac293ba406ea7a6b3c153435527e1837
Bhavye-Malhotra <49281065+Bhavye-Malhotra@users.noreply.github.com>
2020-03-17 18:59:30 +0530

    Add files via upload

---
+++ help_info.py
@@ -0,0 +1,77 @@
+ctftime_help = '''
+`>ctftime upcoming [1-5]`
+return info on a number of upcoming ctfs from ctftime.org
+
+`>ctftime current`
+return info on the currently running ctfs on ctftime.org
+
+`>ctftime [countdown/timeleft]`
+return specific times for the time until a ctf begins, or until a currently running ctf ends
+
+`>ctftime top [year]`
+display the leaderboards from ctftime from a certain year
+'''
+
+utility_help = '''
+
+`>magicb [filetype]`
+return the magicbytes/file header of a supplied filetype.
+
+`>rot "message"`
+return all 25 different possible combinations for the popular caesar cipher - use quotes for messages more than 1 word
+
+`>b64 [encode/decode] "message"`
+encode or decode in base64 - if message has spaces use quotations
+
+`>binary [encode/decode] "message"`
+encode or decode in binary - if message has spaces use quotations
+
+`>hex [encode/decode] "message"`
+encode or decode in hex - if message has spaces use quotations
+
+`>url [encode/decode] "message"`
+encode or decode based on url encoding - if message has spaces use quotations
+
+`>reverse "message"`
+reverse the supplied string - if message has spaces use quotations
+
+`>counteach "message"`
+count the occurences of each character in the supplied message - if message has spaces use quotations
+
+`>characters "message"`
+count the amount of characters in your supplied message
+
+`>wordcount "phrase"`
+count the amount of words in your supplied message
+
+`>atbash "message"`
+encode or decode in the atbash cipher - if message has spaces use quotations (encode/decode do the same thing)
+
+`>github [user]`
+get a direct link to a github profile page with your supplied user
+
+`>twitter [user]`
+get a direct link to a twitter profile page with your supplied user
+
+`>cointoss`
+get a 50/50 cointoss to make all your life's decisions
+
+`>amicool`
+for the truth.
+
+
+'''
+
+
+help_page = '''
+
+`>help ctftime`
+info for all ctftime commands
+
+`>help utility`
+everything else! (basically misc)
+
+'''
+
+
+src = "Under Construction"
---
+++ init.py
@@ -0,0 +1,78 @@
+import asyncio
+import random
+from colorama import Back, Fore, Style
+import sys
+import os
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import help_info
+import auth
+
+#Auth is set up like so:
+#auth_token = 'DISCORD TOKEN'
+#conn = "mongodb+srv://MONGODBUSER:PASSWORD@YOURCLUSTER.mongodb.net"
+
+
+
+client = discord.Client()
+bot = commands.Bot(command_prefix='>')
+extensions = ['encoding_decoding', 'cipher', 'ctfs', 'utility']
+bot.remove_command('help')
+blacklisted = []
+cool_names = ['_Wh1t3r0se_#7515', 'BlackCoffe#2718', 'a.lil.sus#7939'] 
+# This is intended to be able to be circumvented.
+# If you do something like report a bug on  GITHUB.
+
+@bot.event
+async def on_ready():
+    print(('<' + bot.user.name) + ' Online>')
+    print(f"discord.py {discord.__version__}\n")
+    await bot.change_presence(activity=discord.Game(name='>help'))
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Invalid command passed.  Use >help")
+    else:
+        await ctx.send(f"There was an error, sorry!\nIf you think this should be fixed, report it with >report \"what happened\"")
+    
+    print(Style.BRIGHT + Fore.RED + f"Error occured with: {ctx.command}\n{error}\n")
+    print(Style.RESET_ALL)
+
+
+# Sends the github link.
+@bot.command()
+async def source(ctx):
+    await ctx.send(help_info.src)
+
+@bot.command()
+async def help(ctx, page=None):
+    
+    if page == 'ctftime':
+        emb = discord.Embed(description=help_info.ctftime_help, colour=4387968)
+        emb.set_author(name='CTFTime Help')
+    elif page == 'utility':
+        emb = discord.Embed(description=help_info.utility_help, colour=4387968)
+        emb.set_author(name='Utilities Help')
+    
+    else:
+        emb = discord.Embed(description=help_info.help_page, colour=4387968)
+        emb.set_author(name='Werewolf.exe')
+    
+    await ctx.channel.send(embed=emb)
+
+@bot.command()
+async def amicool(ctx):
+    authors_name = str(ctx.author)
+    if any((name in authors_name for name in cool_names)):
+        await ctx.send('You are very cool')
+    else:
+        await ctx.send('lolno')
+        await ctx.send('Psst, kid.  Want to be cool?  Find an issue and report it or request a feature you think would be cool.')
+
+if __name__ == '__main__':
+    sys.path.insert(1, os.getcwd() + '/cogs/')
+    for extension in extensions:
+        bot.load_extension(extension)
+    bot.run(auth.auth_token)
